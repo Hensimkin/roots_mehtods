f9078cbe07bec0d4992bf33d311d3199892fd64a
Hen Simkin <hensi1@ac.sce.ac.il>
2022-05-10 16:52:53 +0300

    bisection method

--- Roots finding.py
+++ Roots finding.py
@@ -1,35 +1,55 @@
+import sympy as sp
+from sympy.utilities.lambdify import lambdify
+
 def polynom(x):
     return (x**4)+(x**3)-(3*(x**2))
 
 
 def Bisection_Method(polynom,start_point,end_point,eps):
+    z=polynom
     bigarray=[]
     size=(abs(start_point)+abs(end_point))/0.1
     num=start_point
-    for i in range(int(size)):
+    for i in range(int(size)+1):
         smallarray = []
         smallarray.append(num)
         smallarray.append(round(polynom(num),4))
         bigarray.append(smallarray)
         num = num + 0.1
         num=round(num,3)
+
     for i in range(1,int(size),1):
         if (bigarray[i][1]*bigarray[i-1][1])<0:
+            print("I", "   ", "A", "   ", "B", "   ", "C", "   ", "F(a)", "   ", "F(b)", "   ", "F(c)")
             Bisection(polynom, bigarray[i-1][0], bigarray[i][0], eps)
-    #for i in range(int(size)):
+    x = sp.symbols('x')
+    f2 = diff(polynom)
+    f3 = lambdify(x, f2)
+    for i in range(int(size)+1):
+        num=round(bigarray[i][0],6)
+        f4=round(f3(num),6)
+        bigarray[i].append(f4)
+
+    for i in range(1, int(size)+1, 1):
+        if (bigarray[i][2] * bigarray[i - 1][2]) < 0:
+            print("I", "   ", "A", "   ", "B", "   ", "C", "   ", "F'(a)", "   ", "F'(b)", "   ", "F'(c)")
+            Bisection(f3, bigarray[i - 1][0], bigarray[i][0], eps)
+
+
+def diff(polynom):
+    x = sp.symbols('x')
+    my_f = polynom(x)
+    return sp.diff(my_f, x)
 
 
 
 
 def Bisection(polynom,a,b,eps):
-    num1=0
-    num2=0
     c=0
     fc=0
     i=1
     counter=0
     temp=0
-    print("I","   ","A","   ","B","   ","C","   ","F(a)","   ","F(b)","   ","F(c)")
     while(abs(a-b) >eps):
         c=round((a+b)/2,6)
         fc=round(polynom(c),6)
@@ -58,36 +78,7 @@ def Bisection(polynom,a,b,eps):
         if counter==2:
             print(i, " ", a, " ", temp, " ", c, " ", round(polynom(a),6), " ", round(polynom(temp),6), " ", round(fc,6))
         i+=1
+    print("The point is: ",c)
 
 
-
-
-"""""
 Bisection_Method(polynom,-3,2,0.0001)
-
-
-for i in range(int(size)):
-print(i+1," ",end=" ")
-for j in range(2):
-    print(bigarray[i][j]," ",end=" ")
-print( )
-"""""
-import sympy as sp
-x =sp.symbols('x')
-my_f=polynom(x)
-print("my_func: ",my_f)
-my_f1=sp.diff( my_f,x)
-print("f' : ",my_f1)
-d1=sp.diff(my_f1)
-print("f'': ",d1)
-
-
-
-
-from sympy.utilities.lambdify import lambdify
-f = (x**4)+(x**3)-(3*(x**2))
-f_prime = f.diff(x)
-print("f : ",f)
-print("f' : ",f_prime)
-f = lambdify(x, f)
-f_prime = lambdify(x, f_prime)
